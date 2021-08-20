f8c7035ed6dcf8e43592e6a4811bc5d404b2dd70
Bernese-Corgi <jnyng0621@gmail.com>
2021-08-20 16:11:40 +0900

    style: Add global style for reset style

---
+++ src/styles/global-style.ts
@@ -0,0 +1,156 @@
+// global-style.ts
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  body,
+  body *,
+  body::before,
+  body::after,
+  body *::before,
+  body *::after {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: #ffffff;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    /* font-family: ; */
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  input {
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+  button {
+    user-select: none;
+    cursor: pointer;
+    outline: none;
+    background: transparent;
+    border: none;
+  }
+  ol, ul, li {
+    list-style: none;
+  }
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+  textarea {
+    resize: none;
+  }
+  svg {
+    pointer-events: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+`;
+
+export default GlobalStyle;
