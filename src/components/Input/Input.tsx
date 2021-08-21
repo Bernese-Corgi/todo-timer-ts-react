1d2ce0352c70b8fe6a1c56da6530669cefec5b3f
Bernese-Corgi <jnyng0621@gmail.com>
2021-08-21 20:27:42 +0900

    feat: Add onCange event props type in Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { InputStyle } from './Input.styled';
 
 type InputProps = {
   type: string;
+  value: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Input = ({ type }: InputProps) => {
-  return (
-    <>
-      <InputStyle type={type} />
-    </>
-  );
+const Input = ({ type, value, onChange }: InputProps) => {
+  return <InputStyle value={value} onChange={onChange} type={type} />;
 };
 
 Input.defaultProps = {
   type: 'text',
+  value: '',
 };
 
 export default Input;
