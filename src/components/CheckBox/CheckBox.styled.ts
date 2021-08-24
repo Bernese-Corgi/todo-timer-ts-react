cfe6165283cf38e9b45a98a00c5e7aaaa67b691a
Bernese-Corgi <jnyng0621@gmail.com>
2021-08-25 02:00:54 +0900

    feat: Apply iconSize props change in CheckBox style component

--- src/components/CheckBox/CheckBox.styled.ts
+++ src/components/CheckBox/CheckBox.styled.ts
@@ -1,27 +1,23 @@
 import styled from 'styled-components';
+import { theme } from 'styles/theme';
+import { handleFontSize, handleMarginSize } from 'utils/style';
+import { CheckBoxWrapperProps } from './CheckBox';
 
-export const CheckBoxWrapper = styled.div`
+export const CheckBoxWrapper = styled.div<CheckBoxWrapperProps>`
   position: relative;
-
-  & svg {
-    width: 100%;
-    height: 100%;
-    padding: 1em;
-    cursor: pointer;
-  }
+  ${theme.flexes.center}
+  width: ${({ iconSize }) => handleFontSize(iconSize)};
+  height: ${({ iconSize }) => handleFontSize(iconSize)};
+  margin: ${({ iconSize }) => handleMarginSize(iconSize)};
 `;
 
 export const CheckBoxInput = styled.input`
   ${({ theme }) => theme.positions.absolute.topLeft}
   z-index: 100;
-  width: 100%;
-  height: 100%;
   margin: 0;
   opacity: 0;
 `;
 
 export const CheckBoxLabel = styled.label`
   ${({ theme }) => theme.positions.absolute.topLeft}
-  width: 100%;
-  height: 100%;
 `;
