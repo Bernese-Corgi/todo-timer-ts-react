8c152ca20965f877d407f0247f9cd806aeaa8439
Bernese-Corgi <jnyng0621@gmail.com>
2021-08-25 17:38:49 +0900

    style: Add hover style in TodoItem

--- src/components/TodoItem/TodoItem.styled.ts
+++ src/components/TodoItem/TodoItem.styled.ts
@@ -1,6 +1,22 @@
+import { IconButtonStyle } from 'components/IconButton/IconButton.style';
 import styled from 'styled-components';
 import { theme } from 'styles/theme';
 
 export const TodoItemWrapper = styled.div`
   ${theme.flexes.row('start')}
+
+  ${IconButtonStyle} {
+    opacity: 0;
+    transition: 200ms;
+    transition-timing-function: ease-in-out;
+    -webkit-transition: 200ms;
+    -webkit-transition-timing-function: ease-in-out;
+  }
+
+  &:hover ${IconButtonStyle} {
+    opacity: 100;
+  }
+  input:focus ${IconButtonStyle} {
+    opacity: 100;
+  }
 `;
