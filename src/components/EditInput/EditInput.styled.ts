702a54e50921f26a72a31a8521daa54308b00651
Bernese-Corgi <jnyng0621@gmail.com>
2021-08-26 23:29:04 +0900

    style: Add focus style, done style and Correct component name in EditInput component

--- src/components/EditInput/EditInput.styled.ts
+++ src/components/EditInput/EditInput.styled.ts
@@ -1,12 +1,17 @@
-import Input from 'components/Input/Input';
+import { InputStyle } from 'components/Input/Input.styled';
 import styled from 'styled-components';
 import { theme } from 'styles/theme';
 import { EditInputProps } from './EditInput';
 
 const { colors } = theme;
 
-export const EditInputStyle = styled(Input)<EditInputProps>`
+export const EditInputStyle = styled(InputStyle)<EditInputProps>`
   &:focus {
-    border-bottom: 0.1rem solid ${colors.gray.dark};
+    border-bottom: 0.1rem solid ${colors.gray.base};
   }
+
+  color: ${({ done }) => (done ? colors.gray.light : 'currentColor')};
+  text-decoration: ${({ done }) => (done ? 'line-through' : 'none')};
 `;
+
+EditInputStyle.displayName = 'EditInputStyle';
