66ef2e5a40b7923191bf4eb8e32be1fae895aa9e
Bernese-Corgi <jnyng0621@gmail.com>
2021-08-30 23:58:21 +0900

    feat: Add function for get dat by device language in WrittenDate component

--- src/components/WrittenDate/WrittenDate.tsx
+++ src/components/WrittenDate/WrittenDate.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const WrittenDate = () => {
-  const today: Date = new Date();
+  const writtenDate: Date = new Date();
 
   const dayNamesKR = ['일', '월', '화', '수', '목', '금', '토'];
+  const dayNamesEN = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-  const day = dayNamesKR[today.getDay()];
+  const getDayByDeviceLanguage = (today: Date) => {
+    if (navigator.language === 'ko-KR') return dayNamesKR[today.getDay()];
+    else return dayNamesEN[today.getDay()];
+  };
 
-  const content: string = `${today
+  const content: string = `${writtenDate
     .toLocaleDateString()
     .replaceAll('.', '/')
-    .replaceAll(' ', '')} ${day}`;
+    .replaceAll(' ', '')} ${getDayByDeviceLanguage(writtenDate)}`;
 
   return <span>{content}</span>;
 };
