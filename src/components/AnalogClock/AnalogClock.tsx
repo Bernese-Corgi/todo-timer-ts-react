42796f914d63b7097615fc8472aeb858754067cb
Bernese-Corgi <jnyng0621@gmail.com>
2021-09-09 23:43:14 +0900

    feat: Add analog date degree util function in Analog clock component

--- src/components/AnalogClock/AnalogClock.tsx
+++ src/components/AnalogClock/AnalogClock.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDateDegree } from 'hooks/useDateDegree';
 import {
   AnalogClockWrapper,
   HandWrapper,
@@ -20,37 +21,7 @@ type initialStateType = {
 
 const AnalogClock = () => {
   const now = new Date();
-  
-  const initialState: initialStateType = {
-    sec: (now.getSeconds() / 60) * 360,
-    min: (now.getMinutes() / 60) * 360 + (now.getSeconds() / 60) * 6,
-    hour: (now.getHours() / 12) * 360 + (now.getMinutes() / 60) * 30,
-  };
-  const [secDegree, setSecDegree] = useState(initialState.sec);
-  const [minDegree, setMinDegree] = useState(initialState.min);
-  const [hourDegree, setHourDegree] = useState(initialState.hour);
-
-  useEffect(() => {
-    let id = setInterval(() => {
-      const now = new Date();
-      const seconds = now.getSeconds();
-      const secondsDegrees = (seconds / 60) * 360;
-
-      const mins = now.getMinutes();
-      const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6;
-
-      const hour = now.getHours();
-      const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30;
-
-      setSecDegree(secondsDegrees);
-      setMinDegree(minsDegrees);
-      setHourDegree(hourDegrees);
-
-      console.log(now, seconds, mins, hour);
-    }, 1000);
-
-    return () => clearInterval(id);
-  }, []);
+  const degrees = useDateDegree(now);
 
   return (
     <AnalogClockWrapper>
@@ -60,9 +31,9 @@ const AnalogClock = () => {
         <OuterMarkThree />
         <InnerClockFace />
         <HandWrapper>
-          <Hour degree={hourDegree} />
-          <Minute degree={minDegree} />
-          <Second degree={secDegree} />
+          <Hour degree={degrees.hourDegree} />
+          <Minute degree={degrees.minDegree} />
+          <Second degree={degrees.secDegree} />
         </HandWrapper>
       </OuterClockFace>
     </AnalogClockWrapper>
