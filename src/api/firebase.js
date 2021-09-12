5243bbda33177c52c919fa0c5553b1e0b3fec418
Bernese-Corgi <jnyng0621@gmail.com>
2021-09-12 21:39:51 +0900

    fix: Remove unused firebase code

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/compat/app";
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,8 +25,6 @@ const firebaseConfig = {
   appId: REACT_APP_FB_APP_ID,
 };
 
-const firebase = require('firebase');
-
 /* --------------------------- Initialize Firebase -------------------------- */
 firebase.initializeApp(firebaseConfig);
 
@@ -34,85 +32,23 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-export default firebase;
+// export default firebase;
 
 /* ---------------------------- language setting ---------------------------- */ 
 auth.useDeviceLanguage();
 
 /* -------------------------- Google Auth Provider -------------------------- */
 // LINK https://firebase.google.com/docs/auth/web/google-signin?hl=ko
-const googleProvider = new firebase.auth.GoogleProvider();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
 // ANCHOR setCustomParameters 내부 prompt??
-googleProvider.setCustomParameters({ prompt: 'select_accout' });
+// googleProvider.setCustomParameters({ prompt: 'select_accout' });
 
 // sign in function
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+export const googleUser = auth.currentUser;
+console.log(googleUser)
 
 // sign out function
 export const signOut = () => auth.signOut();
 
-// sign in persistence setting
-auth.setPersistence(firebase.auth.Auth.Persistence.SESSION);
-
-export const setAuthPersist = (value) => {
-  let  mode =  '';
-  switch (value) {
-    default:
-      case 'local':
-        mode = 'LOCAL';
-        break;
-      case 'session':
-        mode = 'SESSION';
-        break;
-      case 'none':
-        mode = 'NONE';
-      }
-      auth.setPersistence(firebase.auth.Auth.Persistence[mode])
-}
-
-/* -------------------------- email / password auth ------------------------- */
-export const signUpWithEmailAndPassword = async (
-  email,
-  password,
-  additionalData = {}
-) => {
-  try {
-    const { user } = await auth.createUserWithEmailAndPassword(email, password);
-    return await createOrGetAuthUser(user, additionalData);
-  } catch (error) {
-    throw new Error(error.message);
-  }
-}
-
-/* ----------------------------- Firestore Utils ---------------------------- */
-export const createOrGetAuthUser = async (user, additionalData = {}) => {
-  if (!user) {
-    throw new Error('user 값 입력이 필요합니다.')
-  }
-
-  // auth user document reference
-  const userRef = firestore.doc(`Users${user.uid}`)
-  // auth user document snapshot
-  const snapshot = await userRef.get()
-
-  if(!snapshot.exists) {
-    try {
-      const { uid, displayName, email, photoURL } = user;
-      const createdAt = new Date()
-
-      await userRef.set({
-        uid, 
-        displayName,
-        email, 
-        photoURL, 
-        createdAt, 
-        // merge additional data
-        ...additionalData,
-      })
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-  return userRef
-}
 
